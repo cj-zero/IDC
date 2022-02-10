dae480935ded1ea642907de785fc7e1c05ac748f
yangsiming <15118115758@163.com>
2022-02-10 09:34:38 +0800

    Revert "Revert "feat:新增中位机和下位机版本查询接口""
    This reverts commit 0f2ca39739dafa1a32dd72b106332a00b996a942.

---
+++ Server/IDC.Repository/Entities/Material/XWJHardware.cs
@@ -0,0 +1,48 @@
+﻿using IDC.Repository.Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDC.Repository.Entities.Material
+{
+    [ConnectionString("Nsap4MaterialDbContext", "Nsap4MaterialDbContext")]
+    public class XWJHardware
+    {
+        /// <summary>
+        /// 自增Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 下位机硬件版型号
+        /// </summary>
+        public string XWJSn { get; set; }
+
+        /// <summary>
+        /// 别名
+        /// </summary>
+        public string Alias { get; set; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remark { get; set; }
+
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        public string CreateUser { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+
+        /// <summary>
+        /// 修改时间
+        /// </summary>
+        public DateTime UpdateTime { get; set; }
+    }
+}
---
+++ Server/IDC.Repository/Entities/Material/ZWJHardware.cs
@@ -0,0 +1,31 @@
+﻿using IDC.Repository.Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDC.Repository.Entities.Material
+{
+    /// <summary>
+    /// 中位机硬件
+    /// </summary>
+    [ConnectionString("Nsap4MaterialDbContext", "Nsap4MaterialDbContext")]
+    public class ZWJHardware
+    {
+        /// <summary>
+        /// 主键Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 中位机软件版本Id
+        /// </summary>
+        public int ZWJSoftwareVersionId { get; set; }
+
+        /// <summary>
+        /// 中位机硬件序列号
+        /// </summary>
+        public string ZWJSn { get; set; }
+    }
+}
