e40329e813798bee7a1c815948ccb5f169c42748
licong <1229346271@qq.com>
2022-05-16 14:18:19 +0800

    feat:是否通过烤机

---
+++ Server/IDC.Application/Nwcali/NwcaliApp.cs
@@ -0,0 +1,46 @@
+﻿using IDC.Application.Basics;
+using IDC.Application.SSO;
+using IDC.Infrastructure.Returned;
+using IDC.Repository.Dapper;
+using OpenAuth.Repository.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDC.Application.Nwcali
+{
+    public class NwcaliApp : BaseApp
+    {
+        public NwcaliApp(IRepositoryBase repositoryBase, IAuth auth) : base(auth, repositoryBase)
+        {
+            
+        }
+
+        /// <summary>
+        /// 是否通过烤机
+        /// </summary>
+        /// <param name="guid"></param>
+        /// <returns></returns>
+        public async Task<TableData> IsPass(string guid)
+        {
+            TableData result = new TableData();
+            result.Data = false;
+            try
+            {
+                var sql = $"select * from machineinfo where Guid='{guid}' and `Status`=1";
+                var query = (await _repositoryBase.FindAsync<MachineInfo>(sql, null)).ToList();
+                if (query != null && query.Count > 0)
+                {
+                    result.Data = true;
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+            return result;
+        }
+    }
+}
---
+++ Server/IDC.WebApi/Controllers/Nwcali/NwcaliController.cs
@@ -0,0 +1,32 @@
+﻿using IDC.Application.Nwcali;
+using IDC.Infrastructure.Returned;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IDC.WebApi.Controllers.Nwcali
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [ApiExplorerSettings(GroupName = "Nwcali")]
+    public class NwcaliController : ControllerBase
+    {
+        private readonly NwcaliApp _app;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="app"></param>
+        public NwcaliController(NwcaliApp app)
+        {
+            _app = app;
+        }
+
+        [HttpGet]
+        public async Task<TableData> IsPass(string guid)
+        {
+            return await _app.IsPass(guid);
+        }
+    }
+}
