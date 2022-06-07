6c5b0f139271fb6adc831eaa48556261a0c3713f
licong <1229346271@qq.com>
2022-06-07 15:36:17 +0800

    feat:是否通过烤机

---
+++ Server/IDC.Application/Nwcali/NwcaliApp.cs
@@ -0,0 +1,52 @@
+﻿using IDC.Application.Basics;
+using IDC.Application.SSO;
+using IDC.Infrastructure.Returned;
+using IDC.Repository.Dapper;
+using IDC.Repository.Entities.Nwcali;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDC.Application.Nwcali
+{
+    public class NwcaliApp : BaseApp
+    {
+        public NwcaliApp(IRepositoryBase repositoryBase, IAuth auth) : base(auth, repositoryBase)
+        {
+            
+        }
+
+        /// <summary>
+        /// 是否通过烤机
+        /// </summary>
+        /// <param name="guid"></param>
+        /// <returns></returns>
+        public async Task<TableData> IsPass(string guid)
+        {
+            TableData result = new TableData();
+            result.Data = false;
+            try
+            {
+                //下位机最新的烤机环境下烤机记录
+                var sql = $"select EdgeGuid,SrvGuid,DevUid,LowGuid from devicetestlog where LowGuid='{guid}' order by id desc limit 1";
+                var query = (await _repositoryBase.FindAsync<DeviceTestLog>(sql, null)).Select(c=>new  {c.EdgeGuid,c.SrvGuid,c.DevUid,c.LowGuid }).FirstOrDefault();
+                if (query != null)
+                {
+                    //通道烤机结果
+                    var channel = $"select Status,TaskStatus,TaskErrCount from devicetestlog where EdgeGuid='{query.EdgeGuid}' and SrvGuid='{query.SrvGuid}' and DevUid={query.DevUid} and LowGuid='{query.LowGuid}'";
+                    var channelQuery = (await _repositoryBase.FindAsync<DeviceTestLog>(channel, null)).Select(c => new { c.Status, c.TaskStatus, c.TaskErrCount }).ToList();
+                    //状态为已完成，检测结果为完成，错误数量为0
+                    var res = channelQuery.All(c => c.Status == -1 && c.TaskStatus == 2 && c.TaskErrCount == 0);
+                    result.Data = res;
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+            return result;
+        }
+    }
+}
---
+++ Server/IDC.Repository/Entities/Nwcali/DeviceTestLog.cs
@@ -0,0 +1,41 @@
+﻿using IDC.Repository.Dapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace IDC.Repository.Entities.Nwcali
+{
+    [ConnectionString("Nsap4NwcaliDbContext", "Nsap4NwcaliDbContext")]
+    public class DeviceTestLog 
+    {
+        public int Id { get; set; }
+        public string GeneratorCode { get; set; }
+        public string EdgeGuid { get; set; }
+        public string SrvGuid { get; set; }
+        public string BtsServerIp { get; set; }
+        public string MidGuid { get; set; }
+        public string LowGuid { get; set; }
+        public int DevUid { get; set; }
+        public int UnitId { get; set; }
+        public int Status { get; set; }
+        public long TestId { get; set; }
+        public DateTime CreateTime { get; set; }
+        public int ChlId { get; set; }
+        public string CreateUserId { get; set; }
+        public string CreateUser { get; set; }
+        public long OrderNo { get; set; }
+        public string Department { get; set; }
+        public DateTime ChangeStatusTime { get; set; }
+        public int StepId { get; set; }
+        public string CodeTxt { get; set; }
+        public int StepCount { get; set; }
+        public string PrtCode { get; set; }
+        public int MaxRange { get; set; }
+        public string TaskId { get; set; }
+        public sbyte TaskStatus { get; set; }
+        public string TaskContent { get; set; }
+        public int TaskErrCount { get; set; }
+
+    }
+}
